abe9b1e13b04508d4f042635ff6d0b1b3fe9d909
mohamedhamdan2002 <mohamedsmsma3s@gmail.com>
2022-09-20 18:33:26 +0200

    blog api setup

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,11 +37,23 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd party apps
+    "rest_framework", #new
+    "corsheaders",
+    # local
+    "accounts",
+    "posts", 
 ]
+RETST_FRAMEWORK={
+    "DEFAULT_PERMISSION_CLASSES":[
+        "rest_framework.permissions.AllowAny",
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",#new
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -121,3 +133,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL="accounts.CustomUser" #new
+CORS_ORIGIN_WHITELIST=(
+    "http://localhost:3000",
+    "http://localhost:8000",
+)
+CSRF_TRUSTED_ORIGINS=["http://localhost:3000"]
