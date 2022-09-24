7566a2b8deb6e8e8a0b62257047128d2b627a455
mohamedhamdan2002 <mohamedsmsma3s@gmail.com>
2022-09-24 12:37:39 +0200

    final blogapi

--- django_project/settings.py
+++ django_project/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "allauth.socialaccount", #new
     "dj_rest_auth", # new
     "dj_rest_auth.registration", #new
+    "drf_spectacular", #new
     # local
     "accounts",
     "posts", 
@@ -59,6 +60,7 @@ REST_FRAMEWORK={
         "rest_framework.authentication.SessionAuthentication", 
         "rest_framework.authentication.TokenAuthentication", #new
     ],
+    "DEFAULT_SCHEMA_CLASS":"drf_spectacular.openapi.AutoSchema",#new
 }
 
 MIDDLEWARE = [
@@ -153,3 +155,8 @@ CORS_ORIGIN_WHITELIST=(
 CSRF_TRUSTED_ORIGINS=["http://localhost:3000"]
 EMAIL_BACKEND="django.core.mail.backends.console.EmailBackend" #new
 SITE_ID= 1 #new
+SPECTACULAR_SETTINGS={
+    "TITLE":"Blog Api Project",
+    "DESCRIPTION":"a Sample blog to learn about DRF",
+    "VERSION":"1.0.0",
+}
